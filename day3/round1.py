a795bb7cea94acd33d25f6c2f29c48311e561316
Loïc Dias Da Silva <ldiasdasilva@thefork.com>
2022-12-03 19:30:57 +0100

    moved definition of lambda at upper level

--- day3/round1.py
+++ day3/round1.py
@@ -4,16 +4,17 @@
 P = [{ chr(x):x-ord(A)+N for x in range(ord(A), ord(A) + 26) } for A,N in { 'a':1, 'A':27 }.items() ]
 P = P[0] | P[1]
 
-file = open("input.txt", "r")
 total = 0
+cumul = lambda N: lambda x: Occ.update( {x: Occ.get(x, N) + N} )
+
+file = open("input.txt", "r")
 for line in file:
-  l, Occ = len(line), dict() 
+  l, Occ = len(line), dict()
 
   # Cumulate item types by compartments taking advantage of decimal system
-  cumul = lambda N: lambda x: Occ.update( {x: Occ.get(x, N) + N} )
   [ cumul(1)(x) for x in list(line[:l//2]) ]
   [ cumul(100)(x) for x in list(line[l//2:].strip()) ]
-  
+
   # Filter common item types, one and only one should exists
   common = dict(filter(lambda x: ( x[1] > 100 and x[1] % 100 > 0 ), Occ.items()))
   assert len(common) == 1, "none or multiple common items detected"
