c98e1068a7130c7aa0c2fdc3f01c82fd50fa443d
Loïc Dias Da Silva <ldiasdasilva@thefork.com>
2022-12-04 01:57:50 +0100

    pop instead of next(iter( for better readability

--- day3/rounds.py
+++ day3/rounds.py
@@ -9,11 +9,11 @@ total1, total2, group = 0, 0, []
 file = open("input.txt", "r")
 for line in file:
   l = len(line)
-  total1 += P[next(iter( set(line[:l//2]) & set(line[l//2:].strip()) ))]
+  total1 += P[(set(line[:l//2]) & set(line[l//2:].strip())).pop()]
 
   group.append(line.strip())
   if len(group) == 3:
-    total2 += P[next(iter( set(group[0]) & set(group[1]) & set(group[2]) ))]
+    total2 += P[(set(group[0]) & set(group[1]) & set(group[2])).pop()]
     group = []
 
 file.close()
