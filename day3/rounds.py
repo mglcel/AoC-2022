700b3dacb1a11533e1325813cec39a1761cbfe48
Loïc Dias Da Silva <ldiasdasilva@thefork.com>
2022-12-04 02:02:00 +0100

    cleaning

--- day3/rounds.py
+++ day3/rounds.py
@@ -1,7 +1,7 @@
 #!/opt/homebrew/bin/python3
 
 # Build P(riority) = { 'a': 1, ..., 'z': 25, 'A': 26, ..., 'Z': 52 }
-P = [{ chr(x):x-ord(A)+N for x in range(ord(A), ord(A) + 26) } for A,N in { 'a':1, 'A':27 }.items() ]
+P = [{ chr(x):x-ord(A)+N for x in range(ord(A), ord(A) + 26) } for A,N in { 'a':1, 'A':27 }.items()]
 P = P[0] | P[1]
 
 total1, total2, group = 0, 0, []
@@ -9,11 +9,11 @@ total1, total2, group = 0, 0, []
 file = open("input.txt", "r")
 for line in file:
   l = len(line)
-  total1 += P[(set(line[:l//2]) & set(line[l//2:].strip())).pop()]
+  total1 += P[( set(line[:l//2]) & set(line[l//2:].strip()) ).pop()]
 
   group.append(line.strip())
   if len(group) == 3:
-    total2 += P[(set(group[0]) & set(group[1]) & set(group[2])).pop()]
+    total2 += P[( set(group[0]) & set(group[1]) & set(group[2]) ).pop()]
     group = []
 
 file.close()
