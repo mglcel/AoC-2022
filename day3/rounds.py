9d3aa7223268c159ff4638d8db7d6ead57e03699
Loïc Dias Da Silva <ldiasdasilva@thefork.com>
2022-12-04 01:53:11 +0100

    take benefit of sets intersection pythonic syntax

--- day3/rounds.py
+++ day3/rounds.py
@@ -9,11 +9,11 @@ total1, total2, group = 0, 0, []
 file = open("input.txt", "r")
 for line in file:
   l = len(line)
-  total1 += P[next(iter( set( line[:l//2] ).intersection(list( line[l//2:].strip() )) ))]
+  total1 += P[next(iter( set(line[:l//2]) & set(line[l//2:].strip()) ))]
 
   group.append(list(line.strip()))
   if len(group) == 3:
-    total2 += P[next(iter( set(group[0]).intersection(group[1]).intersection(group[2]) ))]
+    total2 += P[next(iter( set(group[0]) & set(group[1]) & set(group[2]) ))]
     group = []
 
 file.close()
