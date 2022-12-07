3feb2c5b707142a7a6ab7f0a63d6ff8c360d6d40
Loïc Dias Da Silva <ldiasdasilva@thefork.com>
2022-12-07 02:37:16 +0100

    simplification

--- day6/rounds.rb
+++ day6/rounds.rb
@@ -3,7 +3,7 @@ $f = File.read("input.txt").split
 
 def compute(s)
   ($f[0].length - (s-1)).times { |i|
-    begin puts i+s; break end if $f[0][i,s].split('').group_by {|c| c.itself}.values.sort_by{|v| -v.length}[0].length == 1
+    begin puts i+s; break end if $f[0][i,s].split('').group_by {|c| c}.values.sort_by{|v| -v.length}[0].length == 1
   }
 end
 
