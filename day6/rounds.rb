1234f99a99b0dd4732ac6bc27fd1ab31340f05b9
Loïc Dias Da Silva <ldiasdasilva@thefork.com>
2022-12-07 02:40:18 +0100

    readability

--- day6/rounds.rb
+++ day6/rounds.rb
@@ -3,7 +3,9 @@ $f = File.read("input.txt").split
 
 def compute(s)
   ($f[0].length - (s-1)).times { |i|
-    begin puts i+s; break end if $f[0][i,s].split('').group_by {|c| c}.values.sort_by{|v| -v.length}[0].length == 1
+    begin 
+      puts i+s; break 
+    end if $f[0][i,s].split('').group_by{|c|c}.values.sort_by{|v|-v.length}[0].length == 1
   }
 end
 
