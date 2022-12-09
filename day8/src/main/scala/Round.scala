2cbe331114821f931f4e630c121b6dc88710512b
Loïc Dias Da Silva <ldiasdasilva@thefork.com>
2022-12-09 02:49:42 +0100

    Renamed functions with relatable names

--- day8/src/main/scala/Round.scala
+++ day8/src/main/scala/Round.scala
@@ -22,30 +22,30 @@ object Rounds {
   private def shift(pos: Coordinates)(implicit direction: Int) =
     (pos._1 + shifts(direction)._1, pos._2 + shifts(direction)._2)
 
-  private def getMaxViewSize(pos:Coordinates)(implicit forest : Forest, direction : Int) : Int = {
+  private def getMaxSpace(pos:Coordinates)(implicit forest : Forest, direction : Int) : Int = {
     if ( forest.hasTreeAt(pos) ) {
       val tree = forest.tree(pos)
       if (tree.space(direction) == -1)
-        tree.space.update(direction, getMaxViewSize(shift(pos)))
+        tree.space.update(direction, getMaxSpace(shift(pos)))
       max(tree.space(direction), tree.height)
     } else -1
   }
 
-  private def fillMaxViewSizes(pos:Coordinates)(implicit forest : Forest) : Tree = {
-    Directions.foreach { implicit direction => getMaxViewSize(pos) }
+  private def fillTreeWithSpaces(pos:Coordinates)(implicit forest : Forest) : Tree = {
+    Directions.foreach { implicit direction => getMaxSpace(pos) }
     forest.tree(pos)
   }
 
-  private def getTreeDistance(pos:Coordinates, height:Int = 0)(implicit forest : Forest, direction : Int) : Int = {
+  private def treeDistance(pos:Coordinates, height:Int = 0)(implicit forest : Forest, direction : Int) : Int = {
     if ( forest.hasTreeAt(pos) )
       if (forest.tree(pos).height < height)
-        1 + getTreeDistance(shift(pos), height)
+        1 + treeDistance(shift(pos), height)
       else 1
     else 0
   }
 
-  private def getScenicScore(pos:Coordinates)(implicit forest : Forest): Int =
-    Directions.map(implicit direction => getTreeDistance(shift(pos), forest.tree(pos).height)).product
+  private def scenicScore(pos:Coordinates)(implicit forest : Forest): Int =
+    Directions.map(implicit direction => treeDistance(shift(pos), forest.tree(pos).height)).product
 
   def main(args: Array[String]): Unit = {
     var treeMatrix = fromFile("input.txt").getLines.toArray
@@ -58,13 +58,13 @@ object Rounds {
 
     val visibleTrees: HashSet[Tree] = HashSet.empty
     for (y <- treeMatrix.indices; x <- treeMatrix(y).indices) {
-      val tree = fillMaxViewSizes((x, y))
+      val tree = fillTreeWithSpaces((x, y))
       if (tree.isVisible) visibleTrees.add(tree)
     }
 
     val scenicScores: SortedSet[Int] = SortedSet.empty
     for (y <- treeMatrix.indices; x <- treeMatrix(y).indices)
-      scenicScores.add(getScenicScore((x, y)))
+      scenicScores.add(scenicScore((x, y)))
 
     println(s"Round 1 : ${visibleTrees.size}")
     println(s"Round 2 : ${scenicScores.last}")
