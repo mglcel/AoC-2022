c4399948f674eeb9c71ef462892afbfa4d949721
Loïc Dias Da Silva <ldiasdasilva@thefork.com>
2022-12-12 00:52:43 +0100

    comestics

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -2,12 +2,12 @@ use std::collections::VecDeque;
 use itertools::Itertools;
 use std::fs;
 
-type Op = Box<(dyn Fn(u32) -> u32 + 'static)>;
+type Op = Box<(dyn Fn(u16) -> u16 + 'static)>;
 
 struct Monkey {
-    items: VecDeque<u32>,
+    items: VecDeque<u16>,
     apply: Option<Op>,
-    test: Option<Box<(dyn Fn(u32) -> bool + 'static)>>,
+    test: Option<Box<(dyn Fn(u16) -> bool + 'static)>>,
     throw_on_true: usize,
     throw_on_false: usize,
     inspected: usize
@@ -20,19 +20,19 @@ impl Default for Monkey {
 
 // ----------------------------------------------------------------------------
 
-fn self_multiply_op() -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x * x)) }
-fn self_sub_op() -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x - x)) }
-fn self_sum_op() -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x + x)) }
-fn self_divide_op() -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x / x)) }
+fn self_multiply_op() -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x * x)) }
+fn self_sub_op() -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x - x)) }
+fn self_sum_op() -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x + x)) }
+fn self_divide_op() -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x / x)) }
 
-fn multiply_op(v: u32) -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x * v.clone())) }
-fn sub_op(v: u32) -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x - v.clone())) }
-fn sum_op(v: u32) -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x + v.clone())) }
-fn divide_op(v: u32) -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x / v.clone())) }
+fn multiply_op(v: u16) -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x * v.clone())) }
+fn sub_op(v: u16) -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x - v.clone())) }
+fn sum_op(v: u16) -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x + v.clone())) }
+fn divide_op(v: u16) -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x / v.clone())) }
 
-fn is_divisible(v: u32) -> Box<dyn Fn(u32) -> bool> { Box::new(move |x: u32| (x % v.clone() == 0)) }
+fn is_divisible(v: u16) -> Box<dyn Fn(u16) -> bool> { Box::new(move |x: u16| (x % v.clone() == 0)) }
 
-fn new_op(op: String, value: String) -> Option<Box<dyn Fn(u32) -> u32>> {
+fn new_op(op: String, value: String) -> Option<Box<dyn Fn(u16) -> u16>> {
     if value == "old" {
         match op.as_str() {
             "*" => Some(self_multiply_op()),
@@ -43,16 +43,41 @@ fn new_op(op: String, value: String) -> Option<Box<dyn Fn(u32) -> u32>> {
         }
     } else {
         match op.as_str() {
-            "*" => Some(multiply_op(value.parse::<u32>().unwrap())),
-            "-" => Some(sub_op(value.parse::<u32>().unwrap())),
-            "+" => Some(sum_op(value.parse::<u32>().unwrap())),
-            "/" => Some(divide_op(value.parse::<u32>().unwrap())),
+            "*" => Some(multiply_op(value.parse::<u16>().unwrap())),
+            "-" => Some(sub_op(value.parse::<u16>().unwrap())),
+            "+" => Some(sum_op(value.parse::<u16>().unwrap())),
+            "/" => Some(divide_op(value.parse::<u16>().unwrap())),
             _ => None
         }
     }
 }
 
 // ----------------------------------------------------------------------------
+
+fn look_at_monkeys(monkeys: &mut Vec<Monkey>, rounds: usize) {
+    for _r in 0..rounds {
+        for i in 0..monkeys.len() {
+            for item in monkeys[i].items.clone().iter() {
+                let new_item = { monkeys[i].apply.as_ref().unwrap()(*item) / 3 };
+                let throw_on = if monkeys[i].test.as_ref().unwrap()(new_item) {
+                    monkeys[i].throw_on_true
+                } else {
+                    monkeys[i].throw_on_false
+                };
+                monkeys[throw_on].items.push_back(new_item);
+            };
+            monkeys[i].inspected += monkeys[i].items.len();
+            monkeys[i].items.truncate(0);
+        }
+    }
+}
+
+fn monkey_business_level(monkeys: &mut Vec<Monkey>) -> usize {
+    monkeys.into_iter()
+        .sorted_by_key(|monkey| -(monkey.inspected as i16))
+        .take(2).map(|x| x.inspected)
+        .product::<usize>()
+}
 
 fn main()-> std::io::Result<()> {
     let mut monkeys : Vec<Monkey> = vec![];
@@ -65,14 +90,14 @@ fn main()-> std::io::Result<()> {
         match i_line % 7 {
             1 => c_monkey.items = VecDeque::from_iter(
                 line.get(18..).unwrap().split(", ")
-                    .map(|s| s.parse::<u32>().unwrap()).collect::<Vec<u32>>()),
+                    .map(|s| s.parse::<u16>().unwrap()).collect::<Vec<u16>>()),
             2 => {
                 let mut params = line.get(23..).unwrap().split(" ");
                 c_monkey.apply = new_op(params.next().unwrap().to_string(),
                                         params.next().unwrap().to_string());
             },
             3 => c_monkey.test =
-                Some(is_divisible(line.get(21..).unwrap().parse::<u32>().unwrap())),
+                Some(is_divisible(line.get(21..).unwrap().parse::<u16>().unwrap())),
             4 => c_monkey.throw_on_true =
                 line.get(29..).unwrap().parse::<usize>().unwrap(),
             5 => c_monkey.throw_on_false =
@@ -84,34 +109,8 @@ fn main()-> std::io::Result<()> {
     }
     monkeys.push(c_monkey);
 
-    for i in 0..20 {
-        for i in 0..monkeys.len() {
-            {
-                println!("Monkey {:?}:", i);
-            }
-            for item in monkeys[i].items.clone().iter() {
-                println!("  Monkey inspects an item with a worry level of : {:?}", item);
-                let new_item = { monkeys[i].apply.as_ref().unwrap()(*item) / 3 };
-                println!("Worry level is transformed to {:?}", new_item);
-                let throw_on = if monkeys[i].test.as_ref().unwrap()(new_item) {
-                    println!("test is statisfied");
-                    monkeys[i].throw_on_true
-                } else {
-                    println!("test is not statisfied");
-                    monkeys[i].throw_on_false
-                };
-                println!("Move to monkey {:?}", throw_on);
-                monkeys[throw_on].items.push_back(new_item);
-            };
-            monkeys[i].inspected += monkeys[i].items.len();
-            monkeys[i].items.truncate(0);
-        }
-    }
+    look_at_monkeys(&mut monkeys, 20);
+    { println!("Round 1 : {:?}", monkey_business_level(&mut monkeys)); }
 
-    {
-        println!("{:?}", monkeys.into_iter().sorted_by_key(|monkey| -(monkey.inspected as i16)).take(2).map(|x| x.inspected).product::<usize>());
-    }
-
-    //println!("{:?}", monkeys.len());
     Ok(())
 }
