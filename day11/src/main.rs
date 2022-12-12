a7c99d88e5362a0e27a69a67cb34d82e54e90083
Loïc Dias Da Silva <ldiasdasilva@thefork.com>
2022-12-12 01:38:56 +0100

    Preparation for round 2, some refactoring needed

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -2,37 +2,40 @@ use std::collections::VecDeque;
 use itertools::Itertools;
 use std::fs;
 
-type Op = Box<(dyn Fn(u16) -> u16 + 'static)>;
+type Op = Box<(dyn Fn(u32) -> u32 + 'static)>;
 
 struct Monkey {
-    items: VecDeque<u16>,
+    items: VecDeque<u32>,
     apply: Option<Op>,
-    test: Option<Box<(dyn Fn(u16) -> bool + 'static)>>,
+    test: Option<Box<(dyn Fn(u32) -> bool + 'static)>>,
     throw_on_true: usize,
     throw_on_false: usize,
     inspected: usize
 }
 impl Default for Monkey {
     fn default () -> Monkey {
-        Monkey{items: VecDeque::new(), apply: None, test: None, throw_on_true: 0, throw_on_false: 0, inspected: 0}
+        Monkey{items: VecDeque::new(), apply: None, test: None,
+            throw_on_true: 0, throw_on_false: 0, inspected: 0}
     }
 }
 
 // ----------------------------------------------------------------------------
 
-fn self_multiply_op() -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x * x)) }
-fn self_sub_op() -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x - x)) }
-fn self_sum_op() -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x + x)) }
-fn self_divide_op() -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x / x)) }
+fn self_multiply_op() -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x * x)) }
+fn self_sub_op() -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x - x)) }
+fn self_sum_op() -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x + x)) }
+fn self_divide_op() -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x / x)) }
 
-fn multiply_op(v: u16) -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x * v.clone())) }
-fn sub_op(v: u16) -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x - v.clone())) }
-fn sum_op(v: u16) -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x + v.clone())) }
-fn divide_op(v: u16) -> Box<dyn Fn(u16) -> u16> { Box::new(move |x: u16| (x / v.clone())) }
+fn multiply_op(v: u32) -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x * v.clone())) }
+fn sub_op(v: u32) -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x - v.clone())) }
+fn sum_op(v: u32) -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x + v.clone())) }
+fn divide_op(v: u32) -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x / v.clone())) }
 
-fn is_divisible(v: u16) -> Box<dyn Fn(u16) -> bool> { Box::new(move |x: u16| (x % v.clone() == 0)) }
+//fn mod_op(v: u32) -> Box<dyn Fn(u32) -> u32> { Box::new(move |x: u32| (x % v.clone())) }
 
-fn new_op(op: String, value: String) -> Option<Box<dyn Fn(u16) -> u16>> {
+fn is_divisible(v: u32) -> Box<dyn Fn(u32) -> bool> { Box::new(move |x: u32| (x % v.clone() == 0)) }
+
+fn new_op(op: String, value: String) -> Option<Box<dyn Fn(u32) -> u32>> {
     if value == "old" {
         match op.as_str() {
             "*" => Some(self_multiply_op()),
@@ -43,10 +46,10 @@ fn new_op(op: String, value: String) -> Option<Box<dyn Fn(u16) -> u16>> {
         }
     } else {
         match op.as_str() {
-            "*" => Some(multiply_op(value.parse::<u16>().unwrap())),
-            "-" => Some(sub_op(value.parse::<u16>().unwrap())),
-            "+" => Some(sum_op(value.parse::<u16>().unwrap())),
-            "/" => Some(divide_op(value.parse::<u16>().unwrap())),
+            "*" => Some(multiply_op(value.parse::<u32>().unwrap())),
+            "-" => Some(sub_op(value.parse::<u32>().unwrap())),
+            "+" => Some(sum_op(value.parse::<u32>().unwrap())),
+            "/" => Some(divide_op(value.parse::<u32>().unwrap())),
             _ => None
         }
     }
@@ -54,11 +57,12 @@ fn new_op(op: String, value: String) -> Option<Box<dyn Fn(u16) -> u16>> {
 
 // ----------------------------------------------------------------------------
 
-fn look_at_monkeys(monkeys: &mut Vec<Monkey>, rounds: usize) {
+fn look_at_monkeys(monkeys: &mut Vec<Monkey>, apply_div: Box<dyn Fn(u32) -> u32>, rounds: i32) {
     for _r in 0..rounds {
         for i in 0..monkeys.len() {
             for item in monkeys[i].items.clone().iter() {
-                let new_item = { monkeys[i].apply.as_ref().unwrap()(*item) / 3 };
+                let new_item:u32 = {
+                    apply_div(monkeys[i].apply.as_ref().unwrap()(*item)) };
                 let throw_on = if monkeys[i].test.as_ref().unwrap()(new_item) {
                     monkeys[i].throw_on_true
                 } else {
@@ -90,14 +94,14 @@ fn main()-> std::io::Result<()> {
         match i_line % 7 {
             1 => c_monkey.items = VecDeque::from_iter(
                 line.get(18..).unwrap().split(", ")
-                    .map(|s| s.parse::<u16>().unwrap()).collect::<Vec<u16>>()),
+                    .map(|s| s.parse::<u32>().unwrap()).collect::<Vec<u32>>()),
             2 => {
                 let mut params = line.get(23..).unwrap().split(" ");
                 c_monkey.apply = new_op(params.next().unwrap().to_string(),
                                         params.next().unwrap().to_string());
             },
             3 => c_monkey.test =
-                Some(is_divisible(line.get(21..).unwrap().parse::<u16>().unwrap())),
+                Some(is_divisible(line.get(21..).unwrap().parse::<u32>().unwrap())),
             4 => c_monkey.throw_on_true =
                 line.get(29..).unwrap().parse::<usize>().unwrap(),
             5 => c_monkey.throw_on_false =
@@ -109,8 +113,13 @@ fn main()-> std::io::Result<()> {
     }
     monkeys.push(c_monkey);
 
-    look_at_monkeys(&mut monkeys, 20);
+    look_at_monkeys(&mut monkeys, divide_op(3), 20);
     { println!("Round 1 : {:?}", monkey_business_level(&mut monkeys)); }
+
+    // TODO: replace test function by a divisor, clean inspected
+    //let div = { monkeys.iter().map(|monkey| monkey.divisor).reduce(|a: u32, b: u32| -> u32 { a * b }) };
+    //look_at_monkeys(&mut monkeys, mod_op(div),  10000);
+    //{ println!("Round 2 : {:?}", monkey_business_level(&mut monkeys)); }
 
     Ok(())
 }
